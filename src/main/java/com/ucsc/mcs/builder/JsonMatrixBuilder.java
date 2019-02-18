76ad52f8c97abdb5516bfb8e6f2522b97cb230ae
sashika <sashikarangoda1@gmail.com>
2019-02-18 06:13:30 +0530

    Added Json Matrix Builder class.

---
+++ src/main/java/com/ucsc/mcs/builder/JsonMatrixBuilder.java
@@ -0,0 +1,13 @@
+package com.ucsc.mcs.builder;
+
+public class JsonMatrixBuilder {
+    private static final int numOfColumns = 100;
+    private static final int numOfRows = 100;
+
+    public void generateMatrix(){
+
+    }
+
+
+    
+}
